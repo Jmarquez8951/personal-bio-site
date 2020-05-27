6b737e95b528ba3a547b096fb2000895f9b4f44d
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-05-27 18:56:31 -0500

    made all things visible from start

--- src/javascripts/components/navToProjects/navToProjects.js
+++ src/javascripts/components/navToProjects/navToProjects.js
@@ -1,10 +1,7 @@
-import utils from '../../helpers/utils';
 import projectCards from '../projectCards/projectCards';
 
 const navToProjects = () => {
   projectCards.createProjectCards();
-  utils.printToDom('technologiesPage', '');
-  utils.printToDom('bioPage', '');
 };
 
 export default { navToProjects };
